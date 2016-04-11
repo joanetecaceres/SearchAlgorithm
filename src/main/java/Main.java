a5aa088147ab5c59de64c1a0deda13c9e789ac2b
Joanete Caceres <jc.caceres@uniandes.edu.co>
2016-04-10 22:36:22 -0500

    archivos tarea 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,73 +1,68 @@
 import static spark.Spark.*;
 import control.Controller;
 import static spark.Spark.get;
-
 /**
  * Esta clase administra la vista de la pagina y muestra los resultados de los casos de prueba
  * Clase  Main
  * @author Carolina Caceres
  */
 public class Main {
-
     /**
      * Metodo main 
      * @param args argumentos
      */
     public static void main(String[] args) {
-
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
-
-        get("/FindValueX", (req, res) -> {
-            
+        get("/FindValueX", (req, res) -> {            
            String resultHtml="<!DOCTYPE html><br>" +
-            "<html><br>" +
-            "<head><br>" +
-            "<style><br>" +
-            "table, th, td {<br>" +
-            "    border: 1px solid gray;<br>" +
-            "    border-collapse: collapse;<br>" +
-            "}<br>" +
-            "th, td {<br>" +
-            "    padding: 5px;<br>" +
-            "}<br>" +
-            "</style><br>" +
-            "</head><br>" +
-            "<body><br>" +
-            "<br>" +
-            "<table style=\"width:100%\"><br>" +
-            "  <tr><br>" +
-            "    <th>p</th><br>" +
-            "    <th>dof</th><br>" +
-            "    <th>Expected Value</th><br>" +
-            "    <th>Actual Value</th><br>" +
-            "   <br>" +
-            "  </tr><br>" +
-            "  <tr><br>" +
-            "    <td>0.20</td><br>" +
-            "    <td>6</td><br>" +
-            "    <td>0.55338</td><br>" +
-            "    <td>"+ Controller.FindValueX(0.20,6,1) +"</td><br>" +
-            "    <br>" +
-            "  </tr><br>" +
-            "  <tr><br>" +
-            "    <td>0.45</td><br>" +
-            "    <td>15</td><br>" +
-            "    <td>1.75305</td><br>" +
-            "    <td>"+  Controller.FindValueX(0.20,6,2)  +"</td><br>" +
-            "    <br>" +
-            "  </tr><br>" +
-            "  <tr><br>" +
-            "    <td>0.495</td><br>" +
-            "    <td>4</td><br>" +
-            "    <td>4.60409</td><br>" +
-            "    <td>"+  Controller.FindValueX(0.20,6,3) +"</td><br>" +
-            "    <br>" +
-            "  </tr><br>" +                        
-            "</table><br>" +
-            "<br>" +
-            "</body><br>" +
-            "</html>";            
+                   "<html><br>" +
+                   "<head><br>" +
+                   "<style><br>" +
+                   "table, th, td {<br>" +
+                   "    border: 1px solid gray;<br>" +
+                   "    border-collapse: collapse;<br>" +
+                   "}<br>" +
+                   "th, td {<br>" +
+                   "    padding: 5px;<br>" +
+                   "}<br>" +
+                   "</style><br>" +
+                   "</head><br>" +
+                   "<body><br>" +
+                   "<br>" +
+                   "<table style=\"width:100%\"><br>" +
+                   "  <tr><br>" +
+                   "    <th>p</th><br>" +
+                   "    <th>dof</th><br>" +
+                   "    <th>Expected Value</th><br>" +
+                   "    <th>Actual Value</th><br>" +
+                   "   <br>" +
+                   "  </tr><br>" +
+                   "  <tr><br>" +
+                   "    <td>0.20</td><br>" +
+                   "    <td>6</td><br>" +
+                   "    <td>0.55338</td><br>" +
+                   "    <td>"+ Controller.FindValueX(0.20,6,1) +"</td><br>" +
+                   "    <br>" +
+                   "  </tr><br>" +
+                   "  <tr><br>" +
+                   "    <td>0.45</td><br>" +
+                   "    <td>15</td><br>" +
+                   "    <td>1.75305</td><br>" +
+                   "    <td>"+  Controller.FindValueX(0.20,6,2)  +"</td><br>" +
+                   "    <br>" +
+                   "  </tr><br>" +
+                   "  <tr><br>" +
+                   "    <td>0.495</td><br>" +
+                   "    <td>4</td><br>" +
+                   "    <td>4.60409</td><br>" +
+                   "    <td>"+  Controller.FindValueX(0.20,6,3) +"</td><br>" +
+                   "    <br>" +
+                   "  </tr><br>" +                        
+                   "</table><br>" +
+                   "<br>" +
+                   "</body><br>" +
+                   "</html>";            
            return resultHtml;            
         });
     }
--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -1,14 +1,12 @@
 package control;
 import model.Operations;
-
 /**
  * clase para gestionar las operaciones para el calculo de la integracion numerica de la distribucion T  
- * y hallar el valor de X más cercano a p
+ * y hallar el valor de X mas cercano a p
  * Clase  Controller
  * @author Carolina Caceres
  */
-public class Controller {
-    
+public class Controller { 
     /**
      * Metodo que retorna el valor de x que es igual al valor p
      * Metodo FindValueX
